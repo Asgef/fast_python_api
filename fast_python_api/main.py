21a2f392049999e5e141aeb440a2d6612636ac9c
Asgef <asgefes1@gmail.com>
2025-02-04 17:43:29 +0300

    refactor project

--- fast_python_api/main.py
+++ fast_python_api/main.py
@@ -1,21 +1,13 @@
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
-from aiohttp import ClientSession
-from fast_python_api.settings import settings
-
+from fast_python_api.services import extermal_api
 
 app = FastAPI()
+
+
+app.include_router(extermal_api.router)
 
 
 @app.get('/')
 async def homepage():
     return JSONResponse({'Hello': 'World'})
-
-
-@app.get('/test')
-async def external_api_test():
-    async with ClientSession() as session:
-        async with session.get(
-                settings.test_service_url, params=settings.param_test_api
-        ) as response:
-            return await response.json()
