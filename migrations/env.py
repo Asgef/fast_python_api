a3843a88163feb728ebcd8e170c3cf1dd361242d
Asgef <asgefes1@gmail.com>
2025-02-28 15:16:09 +0300

    Fix migrations/env.py

--- migrations/env.py
+++ migrations/env.py
@@ -3,11 +3,10 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
-from fast_python_api.database import async_engine
 from fast_python_api.models import Base
-from sqlalchemy.ext.asyncio import async_engine_from_config, AsyncEngine
+from sqlalchemy.ext.asyncio import async_engine_from_config
 from sqlalchemy.engine import Connection
-
+from fast_python_api.settings import settings
 
 target_metadata = Base.metadata
 
@@ -15,6 +14,7 @@ target_metadata = Base.metadata
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
